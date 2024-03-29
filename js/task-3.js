38bdb1bf2842e96ccd3da4abcd945dd2e7c21482
klyuchukviktoriya <klyuchuk.viktoriya@gmail.com>
2024-03-29 02:16:21 +0200

    1

--- js/task-3.js
+++ js/task-3.js
@@ -1,30 +1,15 @@
-function checkForSpam(message) {
-  const normalizedMessage = message.toLowerCase();
-  if (normalizedMessage.includes("spam") || normalizedMessage.includes("sale")) {
-    return true;
-  } else {
-    return false;
-  }
+function filterArray(numbers, value) {
+    const newArray = [];
+    for (let i = 0; i < numbers.length; i += 1) {
+        if (numbers[i] > value) {
+            newArray.push(numbers[i]);
+        }
+    }
+    return newArray;
 }
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
-
-
-
-
-
-
-
-
-// function getElementWidth(content, padding, border) {
-//   return parseFloat(content) + 2 * (parseFloat(padding) + parseFloat(border));
-// }
-// console.log(getElementWidth("50px", "8px", "4px"));
-// console.log(getElementWidth("60px", "12px", "8.5px"));
-// console.log(getElementWidth("200px", "0px", "0px"));
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
